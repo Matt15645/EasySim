83b5766a75878310e14fde57dda6836b8e97cf33
matt <matt910106@gmail.com>
2025-07-12 01:54:14 +0800

    feat(user_service, api_gateway, frontend): finish api_gateway, local test available but backend still have problem

--- backend/api_gateway/src/test/java/com/stock_management/api_gateway/ApiGatewayApplicationTests.java
+++ backend/api_gateway/src/test/java/com/stock_management/api_gateway/ApiGatewayApplicationTests.java
@@ -2,9 +2,7 @@ package com.stock_management.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
